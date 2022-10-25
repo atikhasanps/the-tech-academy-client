2b7507722c28ab40556d2a45fbb86acdf18533e9
atikhasanps <atikhasanps@gmail.com>
2022-10-25 16:41:57 +0600

    login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='mt-8'>
+            <ButtonGroup className='mt-5' vertical>
+      <Button className='mb-2' variant="outline-success"> Login With Email</Button>
+      <Button className='mb-2' variant="outline-primary">Login With Google</Button>
+      <Button variant="outline-dark">Login With GitHub</Button>
+
+     
+    </ButtonGroup>
         </div>
     );
 };
