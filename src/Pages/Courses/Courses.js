b6da8f8816142b8c130f9398b1db34d6496bb0e5
atikhasanps <atikhasanps@gmail.com>
2022-10-25 11:48:18 +0600

    add header

---
+++ src/Pages/Courses/Courses.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const Courses = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col lg="4">
+                            <h2>hello</h2>
+                    </Col>
+                    <Col lg="8">
+                            <h2>hello courses</h2>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Courses;
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Courses from "../../Pages/Courses/Courses";
+
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children:[
+            {
+                path:'/courses',
+                element:<Courses></Courses>
+
+            }
+        ]
+    }
+])
